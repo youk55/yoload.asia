701bd0de099c096582ac5559e24fe172107c5e3e
Yuki Ashizaki <aczaki@gmail.com>
2026-02-10 01:31:31 +0800

    chore: remove contact api debug logs

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -4,9 +4,6 @@ export async function POST(req: Request) {
   try {
     const body = await req.json();
     
-    console.log("BODY:", body)
-	console.log("TOKEN:", body.turnstileToken)
-
 
     const name = (body?.name ?? "").toString().trim();
     const email = (body?.email ?? "").toString().trim();
